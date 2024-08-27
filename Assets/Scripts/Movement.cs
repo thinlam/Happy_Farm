211901a36a34661bf2b42ffcf6e31323d432bd6c
thin lam <thinlam6@gmail.com>
2024-08-28 02:29:46 +0700

    28/8/2024 Start

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    // get input from player
+    // apply movement to sprite 
+
+    private void Update() // 
+    {
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");//Left right (trái phải)
+        
+        float vertical = Input.GetAxisRaw("Vertical");//Up Down (lên xuống) 
+
+        Vector3 direction = new Vector3(horizontal, vertical);
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+}
